4fef68de42a4c4e5c99753d998ab4fff5c0e9789
Zyneeee <157526994+Zyneeee@users.noreply.github.com>
2024-02-17 01:33:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Watching Netflix"];
 
 
 let currentIndex = 0;
